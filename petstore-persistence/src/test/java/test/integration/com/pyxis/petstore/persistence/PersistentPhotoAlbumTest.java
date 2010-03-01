1b908c85fac66d7c6809923e641cb4d722438d9b
Vincent Tencé <vtence@gmail.com>
2010-03-01 10:29:22 -0500

    Using hamcrest assertThat

--- petstore-persistence/src/test/java/test/integration/com/pyxis/petstore/persistence/PersistentPhotoAlbumTest.java
+++ petstore-persistence/src/test/java/test/integration/com/pyxis/petstore/persistence/PersistentPhotoAlbumTest.java
@@ -2,7 +2,7 @@ package test.integration.com.pyxis.petstore.persistence;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.junit.Test;
