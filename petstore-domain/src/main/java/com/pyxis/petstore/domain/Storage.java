e32ac09a7428b4462d572a33d7b14f3f0662eede
Marc-Andre Thibodeau <mathibodeau@pyxis-tech.com>
2010-03-02 15:28:21 -0500

    Added @Service stereotype to Storage interface.

--- petstore-domain/src/main/java/com/pyxis/petstore/domain/Storage.java
+++ petstore-domain/src/main/java/com/pyxis/petstore/domain/Storage.java
@@ -1,8 +1,9 @@
 package com.pyxis.petstore.domain;
 
-//todo add module dependency to spring annotations
+import org.springframework.stereotype.Service;
+
 //todo refactor to photo.getLocation(storage);
-//@Service
+@Service
 public interface Storage {
 
     String getLocation(String attachmentUrl);
--- petstore-domain/src/test/java/test/support/com/pyxis/petstore/builders/EntityBuilder.java
+++ petstore-domain/src/test/java/test/support/com/pyxis/petstore/builders/EntityBuilder.java
@@ -1,6 +1,5 @@
 package test.support.com.pyxis.petstore.builders;
 
-//todo move to domain module to use in domain unit tests
 public interface EntityBuilder<T> {
     T build();
 }
