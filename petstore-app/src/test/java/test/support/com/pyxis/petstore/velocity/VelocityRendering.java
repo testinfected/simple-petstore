97742def0b5c4742f6e30997210762bc61b532ce
Vincent Tencé <vtence@gmail.com>
2010-03-23 15:39:54 -0400

    Using new format for velocity toolbox. Defined a base url to use to construct relative urls. Using lowercase letter in velocity properties now that I found out it's possible.

--- petstore-app/src/test/java/test/support/com/pyxis/petstore/velocity/VelocityRendering.java
+++ petstore-app/src/test/java/test/support/com/pyxis/petstore/velocity/VelocityRendering.java
@@ -1,21 +1,22 @@
 package test.support.com.pyxis.petstore.velocity;
 
-import static org.apache.commons.lang.StringUtils.isEmpty;
+import com.pyxis.petstore.ExceptionImposter;
+import org.apache.velocity.app.VelocityEngine;
+import org.springframework.core.io.UrlResource;
+import org.springframework.ui.velocity.VelocityEngineUtils;
+import org.springframework.web.servlet.view.velocity.VelocityConfigurer;
 
 import java.io.IOException;
 import java.util.Map;
 import java.util.Properties;
 
-import org.apache.velocity.app.VelocityEngine;
-import org.springframework.ui.velocity.VelocityEngineUtils;
-import org.springframework.web.servlet.view.velocity.VelocityConfigurer;
-
-import com.pyxis.petstore.ExceptionImposter;
+import static org.apache.commons.lang.StringUtils.isEmpty;
 
 
 public class VelocityRendering {
 
 	private static final String DEFAULT_ENCODING = "UTF-8";
+    private static final String VELOCITY_CONFIG_FILE_URL_KEY = "velocity.config.url";
 	private static final String TEMPLATES_BASE_URL_KEY = "templates.base.url";
 	private static final String VIEWS_PROPERTIES_FILENAME = "/views.properties";
 	private static final String VELOCITY_EXTENSION = ".vm";
@@ -25,7 +26,7 @@ public class VelocityRendering {
 
 	private String encoding = DEFAULT_ENCODING;
 
-	private VelocityRendering(String template) {
+    private VelocityRendering(String template) {
 		this.template = template;
 	}
 
@@ -38,6 +39,7 @@ public class VelocityRendering {
 	private static void loadVelocityEngine() {
 		try {
 			VelocityConfigurer velocityConfigurer = new VelocityConfigurer();
+            velocityConfigurer.setConfigLocation(new UrlResource(velocityPropertyFileUrl()));
 			velocityConfigurer.setResourceLoaderPath(templatesBaseUrl());
 			velocityConfigurer.afterPropertiesSet();
 			velocityEngine = velocityConfigurer.getVelocityEngine();
@@ -46,13 +48,21 @@ public class VelocityRendering {
 		}
 	}
 
-	private static String templatesBaseUrl() throws IOException {
-		Properties viewsProperties = new Properties();
-		viewsProperties.load(VelocityRendering.class.getResourceAsStream(VIEWS_PROPERTIES_FILENAME));
-		return viewsProperties.getProperty(TEMPLATES_BASE_URL_KEY);
-	}
+    private static String velocityPropertyFileUrl() throws IOException {
+        return getViewProperty(VELOCITY_CONFIG_FILE_URL_KEY);
+    }
 
-	public void withEncoding(String encoding) {
+    private static String templatesBaseUrl() throws IOException {
+        return getViewProperty(TEMPLATES_BASE_URL_KEY);
+    }
+
+    private static String getViewProperty(final String key) throws IOException {
+        Properties properties = new Properties();
+        properties.load(VelocityRendering.class.getResourceAsStream(VIEWS_PROPERTIES_FILENAME));
+        return properties.getProperty(key);
+    }
+
+    public void withEncoding(String encoding) {
 		if(isEmpty(encoding))
 			throw new IllegalArgumentException("Invalid encoding: " + encoding);
 		this.encoding = encoding;
