83f183734eaf1b28b470e60f7000c53d9e600a9d
Vincent Tencé <vtence@gmail.com>
2010-04-07 17:13:13 -0400

    Make sure ItemNumbers with same values are considered equal

--- petstore-domain/src/test/java/test/com/pyxis/petstore/domain/ItemNumberTest.java
+++ petstore-domain/src/test/java/test/com/pyxis/petstore/domain/ItemNumberTest.java
@@ -1,10 +1,14 @@
 package test.com.pyxis.petstore.domain;
+
 import com.pyxis.petstore.domain.ItemNumber;
+import org.hamcrest.Matchers;
 import org.junit.Test;
-import test.support.com.pyxis.petstore.builders.ItemNumberFaker;
 
 import static com.pyxis.matchers.validation.ViolationMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static test.support.com.pyxis.petstore.builders.ItemNumberFaker.aNumber;
 import static test.support.com.pyxis.petstore.validation.ValidationOf.validationOf;
 
 public class ItemNumberTest {
@@ -12,14 +16,26 @@ public class ItemNumberTest {
     String SHOULD_NOT_BE_NULL = "{javax.validation.constraints.NotNull.message}";
 
     @Test public void
-    isValidWithANonNullNumber() {
-        ItemNumber aValidItemNumber = ItemNumberFaker.aNumber();
+    isValidWithANonNullValue() {
+        ItemNumber aValidItemNumber = aNumber();
         assertThat(validationOf(aValidItemNumber), succeeds());
     }
 
     @Test public void
-    isInvalidWithANullNumber() {
-        ItemNumber anItemNumberWithoutANumber = new ItemNumber(null);
-        assertThat(validationOf(anItemNumberWithoutANumber), violates(on("number"), withError(SHOULD_NOT_BE_NULL)));
+    isInvalidWithANullValue() {
+        ItemNumber anEmptyItemNumber = new ItemNumber(null);
+        assertThat(validationOf(anEmptyItemNumber), violates(on("number"), withError(SHOULD_NOT_BE_NULL)));
+    }
+
+    @Test public void
+    isUniquelyIdentifiedByItsValue() {
+        ItemNumber itemNumber = new ItemNumber("12345678");
+        ItemNumber shouldMatch = new ItemNumber("12345678");
+        ItemNumber shouldNotMatch = new ItemNumber("87654321");
+
+        assertThat(itemNumber, equalTo(shouldMatch));
+        assertThat(itemNumber.hashCode(), Matchers.equalTo(shouldMatch.hashCode()));
+        assertThat(itemNumber, not(equalTo(shouldNotMatch)));
+        assertThat(itemNumber.hashCode(), not(equalTo(shouldNotMatch.hashCode())));
     }
 }
