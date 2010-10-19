4e4bb5fb41db4b89069f5ea81e3fc7547eb12831
Vincent Tencé <vtence@gmail.com>
2010-10-19 15:57:59 -0400

    Upgraded to webdriver 2.0a6 to support Firefox 4

--- petstore-system-tests/src/test/java/test/support/com/pyxis/petstore/web/WindowTracker.java
+++ petstore-system-tests/src/test/java/test/support/com/pyxis/petstore/web/WindowTracker.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.WebDriver;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class WindowTracker {
 
@@ -18,9 +21,11 @@ public class WindowTracker {
 
     public WindowTracker(WebDriver webdriver) {
         Class<? extends WebDriver> type = webdriver.getClass();
+        List<Class<?>> interfaces = new ArrayList<Class<?>>(Arrays.asList(type.getInterfaces()));
+        if (!interfaces.contains(WebDriver.class)) interfaces.add(WebDriver.class);
         proxy = WebDriver.class.cast(Proxy.newProxyInstance(
                 type.getClassLoader(),
-                type.getInterfaces(),
+                interfaces.toArray(new Class[interfaces.size()]),
                 new InvocationHandler() {
                     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
                         return forward(method, args);
