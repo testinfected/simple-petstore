de599bf4f178a670dd038e479742694d60e89418
Marc-Andre Thibodeau <mathibodeau@pyxis-tech.com>
2010-12-22 15:28:13 -0500

    Moved clock and nist stuff to 'time' project

--- petstore-infrastructure/src/main/java/com/pyxis/petstore/nist/TimeServerDialect.java
+++
@@ -1,8 +0,0 @@
-package com.pyxis.petstore.nist;
-
-import java.text.ParseException;
-import java.util.Date;
-
-public interface TimeServerDialect {
-    Date translate(String serverOuput) throws ParseException;
-}
