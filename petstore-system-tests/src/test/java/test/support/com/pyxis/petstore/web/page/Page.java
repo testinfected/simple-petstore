0fab1f31997d64e1a5672d06b3c354a23daa0fe1
testinfected <vtence@gmail.com>
2011-09-29 11:29:47 -0400

    Minor cleanups

--- petstore-system-tests/src/test/java/test/support/com/pyxis/petstore/web/page/Page.java
+++ petstore-system-tests/src/test/java/test/support/com/pyxis/petstore/web/page/Page.java
@@ -9,14 +9,4 @@ public abstract class Page {
     protected Page(AsyncWebDriver browser) {
         this.browser = browser;
     }
-
-//    private void turnIntoProperPageSourceMatcher() {
-//        if (webdriver.getTitle().equals("Apache Tomcat/5.5.20 - Error report")) {
-//            Pattern pattern = Pattern.compile("<b>root cause</b>.*?<pre>(.*)</pre>", Pattern.DOTALL);
-//            java.util.regex.Matcher matcher = pattern.matcher(webdriver.getPageSource());
-//            boolean found = matcher.find();
-//            String error = matcher.group(1);
-//            failWith(error);
-//        }
-//    }
 }
--- petstore-system-tests/src/test/java/test/support/com/pyxis/petstore/web/serverdriver/ServerDriverFactory.java
+++ petstore-system-tests/src/test/java/test/support/com/pyxis/petstore/web/serverdriver/ServerDriverFactory.java
@@ -1,7 +1,5 @@
 package test.support.com.pyxis.petstore.web.serverdriver;
 
-import test.support.com.pyxis.petstore.web.serverdriver.ServerDriver;
-
 public interface ServerDriverFactory {
 
     ServerDriver newServerDriver() throws Exception;
