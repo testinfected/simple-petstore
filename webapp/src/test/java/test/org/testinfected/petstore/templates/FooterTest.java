6f9691fc74ee99d524d41e1c77a1d8a803709667
testinfected <vtence@gmail.com>
2012-06-05 15:22:43 -0400

    Consolidate partials into header and footer templates.
    Ensure html is pretty-printed for easier diagnostics.
    
    --HG--
    rename : webapp/src/main/resources/templates/layout/_logout.html => webapp/src/main/resources/templates/layout/footer.html
    rename : webapp/src/main/resources/templates/layout/_logo.html => webapp/src/main/resources/templates/layout/header.html
    rename : webapp/src/test/java/test/org/testinfected/petstore/templates/LogoutTemplateTest.java => webapp/src/test/java/test/org/testinfected/petstore/templates/FooterTest.java
    rename : webapp/src/test/java/test/org/testinfected/petstore/templates/LogoTemplateTest.java => webapp/src/test/java/test/org/testinfected/petstore/templates/HeaderTest.java

--- webapp/src/test/java/test/org/testinfected/petstore/templates/FooterTest.java
+++ webapp/src/test/java/test/org/testinfected/petstore/templates/FooterTest.java
@@ -1,36 +1,36 @@
 package test.org.testinfected.petstore.templates;
 
 import org.hamcrest.Matcher;
+import org.junit.Before;
 import org.junit.Test;
 import org.w3c.dom.Element;
-import test.support.org.testinfected.petstore.templating.Renderer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testinfected.hamcrest.dom.DomMatchers.anElement;
 import static org.testinfected.hamcrest.dom.DomMatchers.hasAttribute;
-import static org.testinfected.hamcrest.dom.DomMatchers.hasChild;
+import static org.testinfected.hamcrest.dom.DomMatchers.hasChildren;
 import static org.testinfected.hamcrest.dom.DomMatchers.hasSelector;
 import static org.testinfected.hamcrest.dom.DomMatchers.hasTag;
 import static test.support.org.testinfected.petstore.templating.Renderer.render;
 
-public class LogoutTemplateTest {
+public class FooterTest {
 
     Element content;
 
-    @SuppressWarnings("unchecked")
-    @Test public void
-    displaysLogoutForm() {
-        content = renderLogoutTemplate().asDom();
+    @Before public void
+    renderContent() {
+        content = render("layout/footer").asDom();
+    }
+
+    @SuppressWarnings("unchecked") @Test public void
+    logoutButtonSubmitsADeleteToLogoutPath() {
         assertThat("content", content, hasLogoutForm(hasAttribute("action", "/logout"), hasAttribute("method", "post")));
-        assertThat("content", content, hasLogoutForm(hasChild(
-                anElement(hasTag("input"), hasAttribute("type", "hidden"), hasAttribute("name", "_method"), hasAttribute("value", "delete")))));
+        assertThat("content", content, hasLogoutForm(hasChildren(
+                anElement(hasTag("input"), hasAttribute("type", "hidden"), hasAttribute("name", "_method"), hasAttribute("value", "delete")),
+                anElement(hasTag("button")))));
     }
 
     private Matcher<Element> hasLogoutForm(Matcher<Element>... formMatchers) {
         return hasSelector("form#logout", formMatchers);
-    }
-
-    private Renderer renderLogoutTemplate() {
-        return render("layout/_logout");
     }
 }
--- webapp/src/test/java/test/org/testinfected/petstore/templates/HeaderTest.java
+++ webapp/src/test/java/test/org/testinfected/petstore/templates/HeaderTest.java
@@ -1,25 +1,25 @@
 package test.org.testinfected.petstore.templates;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.w3c.dom.Element;
-import test.support.org.testinfected.petstore.templating.Renderer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testinfected.hamcrest.dom.DomMatchers.hasAttribute;
 import static org.testinfected.hamcrest.dom.DomMatchers.hasUniqueSelector;
 import static test.support.org.testinfected.petstore.templating.Renderer.render;
 
-public class LogoTemplateTest {
+public class HeaderTest {
 
     Element content;
 
-    @Test public void
-    linksToHome() {
-        content = renderLogoPartial().asDom();
-        assertThat("content", content, hasUniqueSelector("a", hasAttribute("href", "/")));
+    @Before public void
+    renderContent() {
+        content = render("layout/header").asDom();
     }
 
-    private Renderer renderLogoPartial() {
-        return render("layout/_logo");
+    @Test public void
+    logoLinksToHome() {
+        assertThat("content", content, hasUniqueSelector("#logo a", hasAttribute("href", "/")));
     }
 }
