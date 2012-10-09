aff4f0ebfbed71c4f1072ddb4a1cf8d279454990
testinfected <vtence@gmail.com>
2012-10-08 22:24:31 -0400

    Remove todo

--- webapp/src/test/java/test/support/org/testinfected/petstore/web/MockResponse.java
+++ webapp/src/test/java/test/support/org/testinfected/petstore/web/MockResponse.java
@@ -3,7 +3,6 @@ package test.support.org.testinfected.petstore.web;
 import org.simpleframework.http.Response;
 import org.simpleframework.http.ResponseWrapper;
 
-//todo use where possible instead of using dynamic mocks
 public class MockResponse extends ResponseWrapper {
 
     private static final Response DUMMY_RESPONSE = null;
