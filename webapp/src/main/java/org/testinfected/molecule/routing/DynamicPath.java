f21732b75d794a3e78d645a3759671d3241b8635
testinfected <vtence@gmail.com>
2013-04-18 16:32:00 -0400

    Make code parsing dynamic parameters independent of simpleweb and a bit cleaner

--- webapp/src/main/java/org/testinfected/molecule/routing/DynamicPath.java
+++ webapp/src/main/java/org/testinfected/molecule/routing/DynamicPath.java
@@ -1,7 +1,5 @@
 package org.testinfected.molecule.routing;
 
-import org.simpleframework.http.Path;
-import org.simpleframework.http.parse.PathParser;
 import org.testinfected.molecule.util.Matcher;
 
 import java.util.ArrayList;
@@ -11,39 +9,20 @@ import java.util.Map;
 
 public class DynamicPath implements Matcher<String> {
 
-    private final String pattern;
+    private final Path pattern;
 
     public DynamicPath(String pattern) {
-        this.pattern = pattern;
+        this.pattern = new Path(pattern);
     }
 
     public boolean matches(String actual) {
-        return sameLength(segments(pattern), segments(actual)) && match(segments(pattern), segments(actual));
-    }
+        Path path = new Path(actual);
+        if (!pattern.sameLengthAs(path)) return false;
 
-    private boolean match(String[] expectedSegments, String[] actualSegments) {
-        for (int i = 0; i < expectedSegments.length; i++) {
-            String expected = expectedSegments[i];
-            String actual = actualSegments[i];
-            if (!(expected.equals(actual) || isDynamic(expected))) return false;
+        for (int i = 0; i < pattern.segmentCount(); i++) {
+            if (!isDynamic(pattern.segment(i)) && !pattern.segment(i).equals(path.segment(i))) return false;
         }
         return true;
-    }
-
-    private boolean sameLength(String[] expected, String[] actual) {
-        return expected.length == actual.length;
-    }
-
-    private String[] segments(String path) {
-        return removeEmptyParts(path.split("/"));
-    }
-
-    private String[] removeEmptyParts(String[] parts) {
-        List<String> significantParts = new ArrayList<String>();
-        for (String part : parts) {
-            if (!part.isEmpty()) significantParts.add(part);
-        }
-        return significantParts.toArray(new String[significantParts.size()]);
     }
 
     private boolean isDynamic(String segment) {
@@ -51,18 +30,52 @@ public class DynamicPath implements Matcher<String> {
     }
 
     public Map<String, String> boundParameters(String path) {
-        Path actual = new PathParser(path);
+        Path p = new Path(path);
         Map<String, String> boundParameters = new HashMap<String, String>();
 
-        String[] parts = segments(this.pattern);
-        for (int i = 0; i < parts.length; i++) {
-            String segment = parts[i];
-            if (isDynamic(segment)) boundParameters.put(stripLeadingColonFrom(segment), actual.getSegments()[i]);
+        for (int i = 0; i < pattern.segmentCount(); i++) {
+            String segment = pattern.segment(i);
+            if (isDynamic(segment)) {
+                boundParameters.put(stripLeadingColon(segment), p.segment(i));
+            }
         }
         return boundParameters;
     }
 
-    private String stripLeadingColonFrom(String segment) {
+    private String stripLeadingColon(String segment) {
         return segment.substring(1);
+    }
+
+    public static class Path {
+
+        private final String path;
+
+        public Path(String path) {
+            this.path = path;
+        }
+
+        public String[] segments() {
+            return removeEmptyParts(path.split("/"));
+        }
+
+        private static String[] removeEmptyParts(String[] parts) {
+            List<String> segments = new ArrayList<String>();
+            for (String part : parts) {
+                if (!part.isEmpty()) segments.add(part);
+            }
+            return segments.toArray(new String[segments.size()]);
+        }
+
+        public boolean sameLengthAs(Path other) {
+            return other.segments().length == segments().length;
+        }
+
+        public String segment(int index) {
+            return segments()[index];
+        }
+
+        public int segmentCount() {
+            return segments().length;
+        }
     }
 }
